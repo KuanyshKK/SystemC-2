685526d572c8f4bdf54dd06d9efde9d1162f09b7
Куаныш Калиханов <@local.itstep>
2019-02-02 13:55:47 +0600

    Добавьте файлы проекта.

---
+++ ConsoleTcpServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                TcpListener tcpListener =
+             new TcpListener(IPAddress.Parse("127.0.0.1"), 34561);
+                tcpListener.Start();
+                Console.WriteLine("Listening ...");
+
+                TcpClient client = tcpListener.AcceptTcpClient();
+
+                Console.WriteLine("IPAdress: {0}, Port: {1}",
+                    ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString(),
+                    ((IPEndPoint)client.Client.RemoteEndPoint).Port.ToString());
+
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] data = new byte[1024];
+                    stream.Read(data, 0, data.Length);
+
+                    Console.WriteLine("Data: {0}",
+                        Encoding.UTF8.GetString(data));
+                }
+
+                tcpListener.Stop();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         
+        }
+    }
+}
---
+++ ConsoleTopClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTopClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint endPoint =
+             new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1115);
+
+                TcpClient tcpClient = new TcpClient(endPoint);
+
+                tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 34561);
+
+                Console.WriteLine("Write message: ");
+                byte[] data = Encoding.UTF8.GetBytes(Console.ReadLine());
+
+                NetworkStream networkStream = tcpClient.GetStream();
+                networkStream.Write(data, 0, data.Length);
+
+                tcpClient.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         
+        }
+    }
+}
